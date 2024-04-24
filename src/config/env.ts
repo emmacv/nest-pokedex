c49f518c583b3d4e582049c602d22cf26621115b
Emmanuel Canto Vazquez <emikogears134@gmail.com>
2024-04-24 13:52:04 -0600

    FIX: remove MONGO_DB

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,7 @@
 const EnvConfig = () => ({
   PORT: process.env.PORT || 3000,
   ENV: process.env.NODE_ENV,
-  MONGO_DB_URI: process.env.MONGO_DB_URI,
-  SECRET: process.env.A_SECRET_KEY,
+  MONGO_DB: process.env.MONGO_DB,
 });
 
 export default EnvConfig;
